9c0a1ed6587dd7a88949c68b55b2823c99a5560b
developerprince-initd <prince@initd-it.co.za>
2023-11-04 02:22:08 +0200

    Adjusted and cleaned up

--- mschatbot/bot.py
+++ mschatbot/bot.py
@@ -15,7 +15,7 @@ class MyBot(ActivityHandler):
     async def on_message_activity(self, turn_context: TurnContext):
         input_ids = tokenizer.encode(turn_context.activity.text, return_tensors='pt')
 
-        output = model.generate(input_ids, max_length=100, pad_token_id=tokenizer.eos_token_id)
+        output = model.generate(input_ids, max_length=512, pad_token_id=tokenizer.eos_token_id)
 
         output_text=tokenizer.decode(output[:,input_ids.shape[-1]:][0],skip_special_tokens=True)
 
@@ -28,4 +28,4 @@ class MyBot(ActivityHandler):
     ):
         for member_added in members_added:
             if member_added.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("Hello and welcome!")
+                await turn_context.send_activity("Hi there you can try asking me anything")
--- websocket.py
+++ websocket.py
@@ -1,8 +1,7 @@
 import asyncio
 import websockets
 from transformers import AutoTokenizer, AutoModelForCausalLM
-# tokenizer = AutoTokenizer.from_pretrained("cerebras/Cerebras-GPT-1.3B")
-# model = AutoModelForCausalLM.from_pretrained("cerebras/Cerebras-GPT-1.3B")
+
 
 print("Initializaing model...................")
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
@@ -13,7 +12,6 @@ async def generate(websocket):
     while True:
         input_message = await websocket.recv()
         print(input_message)
-        # await websocket.send(input_message)
         input_ids = tokenizer.encode(input_message, return_tensors='pt')
         output = model.generate(input_ids, max_length=2048, pad_token_id=tokenizer.eos_token_id)
         output_text=tokenizer.decode(output[:,input_ids.shape[-1]:][0],skip_special_tokens=True)
